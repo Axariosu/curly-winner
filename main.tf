ef9dc5c3df9759529ed96ade21cc25c9e8211889
Jeffrey Luo <jeffrluo@cisco.com>
2022-09-08 08:39:01 -0700

    test

--- main.tf
+++ main.tf
@@ -9,17 +9,7 @@ resource "null_resource" "trigger" {
   triggers = {
     random = random_id.random.hex
   }
-  # provisioner "local-exec" {
-  #   command = "/bin/bash close_change2.sh"
-  #   environment = {
-  #     snow_instance = var.snow_instance
-  #     username      = var.username
-  #     password      = var.password
-  #     client_id     = var.client_id
-  #     client_secret = var.client_secret
-  #     test          = "test"
-  #   }
-  # }
+  break
 }
 
 resource "null_resource" "depends_on_test" {
