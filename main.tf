b1c4ffafbf0ab9249310d7a901534f77019c630d
Jeffrey Luo <jeffrluo@cisco.com>
2022-09-08 08:34:22 -0700

    test

--- main.tf
+++ main.tf
@@ -9,8 +9,25 @@ resource "null_resource" "trigger" {
   triggers = {
     random = random_id.random.hex
   }
+  # provisioner "local-exec" {
+  #   command = "/bin/bash close_change2.sh"
+  #   environment = {
+  #     snow_instance = var.snow_instance
+  #     username      = var.username
+  #     password      = var.password
+  #     client_id     = var.client_id
+  #     client_secret = var.client_secret
+  #     test          = "test"
+  #   }
+  # }
+}
+
+resource "null_resource" "depends_on_test" {
+  depends_on = [
+    null_resource.trigger
+  ]
   provisioner "local-exec" {
-    command = "/bin/bash close_change2.sh"
+    command = "/bin/bash close_change.sh"
     environment = {
       snow_instance = var.snow_instance
       username      = var.username
@@ -20,14 +37,4 @@ resource "null_resource" "trigger" {
       test          = "test"
     }
   }
-}
-
-resource "random_id" "depends_on_test" {
-  depends_on = [
-    null_resource.trigger
-  ]
-  keepers = {
-    uuid = uuid()
-  }
-  byte_length = 8
 }
